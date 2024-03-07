0b80c9a526afc998539f1c1c99ba95a4af4dd33c
Pittab <120305267+Pittab@users.noreply.github.com>
2024-03-07 09:45:36 +0000

    Create menu.py
    To select gamemmode

---
+++ menu.py
@@ -0,0 +1,21 @@
+import time
+print("welcome to pidms, choose the difficulty of the game here:")
+print()
+print("Easy (1) ")
+print("Regular (2) ")
+print("Hard (3) ")
+
+difsel=input("choose difficulty: ")
+
+if difsel == 1
+ print("welcome to riviting gameplay")
+ sleep(1)
+ import pidmseasy
+elif difsel == 2:
+ print("welcome to riviting gameplay")
+ sleep(1)
+ import pidmsmedium
+else
+ print("welcome to riviting gameplay")
+ sleep(1)
+ import pidmshard
